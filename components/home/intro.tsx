a65c06548f72dcd9fc567eba6c0b1198cc03583e
pantaleone-ai <mdptrading@gmail.com>
2025-08-06 18:28:47 -0600

    cut import { cn } from "@/lib/utils"; from intro

--- components/home/intro.tsx
+++ components/home/intro.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Card from "@/components/ui/card";
-import { cn } from "@/lib/utils";
 import dynamic from "next/dynamic";
 import Image from "next/image";
 import Link from "next/link";

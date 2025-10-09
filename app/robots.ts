1ac6e45fdcdbace2acaf2303c497fdc9131bc586
pantaleone.net <96348408+pantaleone-ai@users.noreply.github.com>
2025-10-09 12:53:42 -0600

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: ["/"],
-      disallow: ["/search?q=", "/admin/"],
+      disallow: ["/search?q=", "/admin/", "/privacy", "/terms"],
     },
     sitemap: ["https://pantaleone.net/sitemap.xml"],
   };
